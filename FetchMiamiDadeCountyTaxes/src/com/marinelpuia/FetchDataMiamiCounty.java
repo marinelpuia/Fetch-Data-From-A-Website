6e4adbbba83f85dbe61307893916762074504d04
Marinel Puia <megatron1975@yahoo.co.uk>
2020-07-08 04:31:16 +0100

    Added a for loop to print out all companies found o the first page

--- FetchMiamiDadeCountyTaxes/src/com/marinelpuia/FetchDataMiamiCounty.java
+++ FetchMiamiDadeCountyTaxes/src/com/marinelpuia/FetchDataMiamiCounty.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class FetchDataMiamiCounty {
@@ -14,7 +15,7 @@ public class FetchDataMiamiCounty {
 		try {
 		
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("Please enter a name:\n");
+			System.out.println("Please enter a name or account number:\n");
 			
 			String USER_SEARCH_KEY_STRING = scanner.nextLine();
 			scanner.close();
@@ -23,16 +24,46 @@ public class FetchDataMiamiCounty {
 	        
 	        Document document = Jsoup.connect(URL + USER_SEARCH_KEY_STRING).referrer(URL + USER_SEARCH_KEY_STRING).get();
 	        
+	        // link to open more account details
 	        Elements businessAccount = document.select("div.identifier");
 	        String businessAccountLink = businessAccount.select("a").first().text();
 	        
 	        System.out.println("\n\t" + businessAccountLink);
 	        System.out.println("\tMailing address: ");
 	        
+	        // address of business
 	        Elements detailsOfBusiness = document.select("div.data-address");
 	        String nameAndAddress = detailsOfBusiness.select("div.data-address").first().text();
 	        
-	        System.out.println("\t" + nameAndAddress);
+	        // account details
+	        Elements accountDetails = document.select("div.actions");
+	        String accDetails = accountDetails.select("div.actions").first().text();
+	        
+	        // business profile
+	        Elements businessProfile= document.select("div.data-qualifier");
+	        String occupationLabel = businessProfile.select("div.headers").first().text();
+	        String occupation = businessProfile.select("div.data-qualifier > p").first().text();
+	        
+	        // pay bill button container	        
+	        Elements billStatus = document.select("div.pay-bill-button-container");
+	        String bill = billStatus.select("div.pay-bill-button-container").first().text();
+	        
+	        
+	        System.out.println("\t" + nameAndAddress + "\n\t" + accDetails + "\n\t" + occupationLabel + ": " + occupation + "\n\t" + bill);
+	        
+	        System.out.println("\n*************** ALL COMPANIES FOUND ON THIS PAGE ***************");
+	        
+	        // print out all companies found on the first page using for loop
+	        Elements allBusinessAcc = document.select("div.result");
+	        int i = 0;
+	        for(Element businessList: allBusinessAcc) {
+	        	i++;
+	        	System.out.println("\n(" + i + ") " + businessList.select("a").first().text() + " \n\t "
+	        	                                    + businessList.select("div.data-address").first().text() + " \n\t "
+	        	                                    + businessList.select("div.actions").first().text() + " \n\t "
+	        	                                    + occupationLabel + ": " + businessList.select("div.data-qualifier > p").first().text() + " \n\t "
+	        	                                    + businessList.select("div.pay-bill-button-container").first().text()); 	
+	        }
 	        
 	        
 			} catch(IOException e) {
